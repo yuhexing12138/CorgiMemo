c5786c5f3f11457b0337fd94f5146f9cb1b7b2dc
yuanhexing12138 <1943994194@qq.com>
2026-07-17 12:31:19 +0800

    chore: 临时删除三个库（准备改用 submodule 管理）

--- Reorderable/demoApp/composeApp/src/macosMain/kotlin/sh/calvin/reorderable/demo/haptics.macos.kt
+++
@@ -1,8 +0,0 @@
-package sh.calvin.reorderable.demo
-
-import androidx.compose.runtime.Composable
-
-@Composable
-actual fun rememberReorderHapticFeedback(): ReorderHapticFeedback {
-    return ReorderHapticFeedback()
-}
